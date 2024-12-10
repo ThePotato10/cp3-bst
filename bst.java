0603e49f13a5cbda6e3ac7b584445ae670823b38
ThePotato10 <63891976+ThePotato10@users.noreply.github.com>
2024-12-10 09:49:22 -0500

    Create bst.java

---
+++ bst.java
@@ -0,0 +1,21 @@
+class BST {
+    public BST() {
+         root = null;
+    }
+  
+    void insert(int key) {
+
+    }
+
+    boolean search(int key) {
+
+    }
+
+    int remove(int key) {
+
+    }
+
+    public String toString() {
+        
+    }
+}
