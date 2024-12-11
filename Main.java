4e4015c8d0786ca20b7e57917aa08e859978c56f
ThePotato10 <63891976+ThePotato10@users.noreply.github.com>
2024-12-11 04:32:16 +0000

    Added toString method

--- Main.java
+++ Main.java
@@ -4,8 +4,11 @@ public class Main {
 
         tree.insert(6, null);
         tree.insert(3, null);
+        tree.insert(7, null);
         tree.insert(5, null);
+        tree.insert(1, null);
+        tree.insert(2, null);
 
-        System.out.println(tree.root.left.right.key);
+        System.out.println(tree.toString());
     }
 }
--- bst.java
+++ bst.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class BST {
     public Node root;
 
@@ -8,24 +10,22 @@ class BST {
     void insert(int key, Node curr) {
         if (root == null) {
             root = new Node(key);
+
         } else {
             if (curr == null) curr = root;
             
             if (key < curr.key) {
                 // Go left
-                if (curr.left == null) {
-                    curr.left = new Node(key);
-                } else {
-                    insert(key, curr.left);
-                }
+                if (curr.left == null) curr.left = new Node(key);
+                else insert(key, curr.left);
+                
             } else {
                 // Go right
-                if (curr.right == null) {
-                    curr.right = new Node(key);
-                } else {
-                    insert(key, curr.right);
-                }
+                if (curr.right == null) curr.right = new Node(key);
+                else insert(key, curr.right);
+
             }
+
         }
     }
 
@@ -35,9 +35,30 @@ class BST {
 
     int remove(int key) {
 
-    }
+    }*/
 
     public String toString() {
+        String treeString = "";
+        ArrayList<Node> currLayer = new ArrayList<Node>();
         
-    }*/
+        currLayer.add(root);
+
+        while (!currLayer.stream().allMatch(x -> x == null)) {
+            ArrayList<Node> nextLayer = new ArrayList<Node>();
+
+            for (int i = 0; i < currLayer.size(); i++) {
+                if (currLayer.get(i) != null) {
+                    treeString += Integer.toString(currLayer.get(i).key) + " ";
+
+                    nextLayer.add(currLayer.get(i).left);
+                    nextLayer.add(currLayer.get(i).right);
+                }
+            }
+
+            currLayer = nextLayer;
+            treeString += "\n";
+        }
+
+        return treeString;
+    }
 }
